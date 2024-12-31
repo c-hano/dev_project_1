47d1c6b65415e0b0747cdaff8b5fddc08adb3040
c-hano <wdq0304@naver.com>
2024-12-31 20:36:16 +0900

    set order entity

---
+++ src/main/java/project/demo/entity/Order.java
@@ -0,0 +1,27 @@
+package project.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long order_id;
+
+    private String customer_email;
+    private LocalDateTime create_at;
+    private int total_price;
+    private String order_state;
+    private Long delivery_id;
+
+}
--- src/main/java/project/demo/entity/Product.java
+++ src/main/java/project/demo/entity/Product.java
@@ -1,11 +1,10 @@
 package project.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -14,11 +13,13 @@ public class Product {
     @Id // 해당 필드가 테이블의 기본 키(primary key) 임을 나타냄.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     // 기본 키 값을 데이터베이스에서 자동으로 생성
-    private Long productId;
+    private Long product_id;
 
-    private String productName;
-    private int productPrice;
-    private String productDescription;
-    private int productStock;
+    private String product_name;
+    private int product_price;
 
+    @Column(length = 500)
+    private String product_description;
+    private int product_stock;
+    private LocalDateTime created_at;
 }

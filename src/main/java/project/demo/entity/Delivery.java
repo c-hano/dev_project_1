d7e7c094f20895ad2254aec4f796d13be9575c0c
c-hano <wdq0304@naver.com>
2025-01-01 15:39:15 +0900

    set Delivery entity

---
+++ src/main/java/project/demo/entity/Delivery.java
@@ -0,0 +1,23 @@
+package project.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int delivery_id;
+
+    private int delivery_number;
+    private String delivery_company;
+    private String delivery_address;
+
+}

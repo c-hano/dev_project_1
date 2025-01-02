dc44845ca38f1d6a17078f83350f2a01b63faa1c
c-hano <wdq0304@naver.com>
2025-01-02 11:44:41 +0900

    set ProductService

--- src/main/java/project/demo/product/ProductService.java
+++ src/main/java/project/demo/product/ProductService.java
@@ -1,4 +1,25 @@
 package project.demo.product;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //상품 목록 조회
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    //상품 추가
+    public ProductEntity createProduct(ProductEntity product) {
+        return productRepository.save(product);
+    }
 }
